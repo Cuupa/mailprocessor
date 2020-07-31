87ab2632f6a98928c9cba5735c63013ae0987995
simon <simon.thiel2+github@gmail.com>
2020-07-31 20:26:09 +0200

    only mark as read if it's configured that way

--- src/main/java/com/cuupa/mailprocessor/delegates/ArchivingSuccessDelegate.kt
+++ src/main/java/com/cuupa/mailprocessor/delegates/ArchivingSuccessDelegate.kt
@@ -4,13 +4,13 @@ import com.cuupa.mailprocessor.MailprocessorConfiguration
 import com.cuupa.mailprocessor.process.ProcessInstanceHandler
 import com.cuupa.mailprocessor.services.input.email.EmailService
 import com.cuupa.mailprocessor.services.input.scan.ScanService
-import com.cuupa.mailprocessor.userconfiguration.UserConfiguration
+import com.cuupa.mailprocessor.userconfiguration.EmailProperties
+import com.cuupa.mailprocessor.userconfiguration.ScanProperties
 import org.apache.juli.logging.LogFactory
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.JavaDelegate
 
-class ArchivingSuccessDelegate(private val scanService: ScanService,
-                               private val emailService: EmailService,
+class ArchivingSuccessDelegate(private val scanService: ScanService, private val emailService: EmailService,
                                private val configuration: MailprocessorConfiguration) : JavaDelegate {
 
     override fun execute(delegateExecution: DelegateExecution) {
@@ -20,22 +20,20 @@ class ArchivingSuccessDelegate(private val scanService: ScanService,
         }
         val configurationForUser = configuration.getConfigurationForUser(handler.username)
         if (handler.isScanMail) {
-            processScanSuccess(configurationForUser, handler)
+            processScanSuccess(configurationForUser.scanProperties, handler)
         } else {
-            processMailSuccess(handler, configurationForUser)
+            processMailSuccess(configurationForUser.emailProperties,handler)
         }
         log.warn("Successfully archived ${handler.fileName} to ${handler.archivedFilename}")
     }
 
-    private fun processMailSuccess(handler: ProcessInstanceHandler, configurationForUser: UserConfiguration) {
-        emailService.markMailAsRead(handler.emailSubject,
-                                    handler.emailLabel,
-                                    handler.receivedDate,
-                                    configurationForUser.emailProperties)
+    private fun processMailSuccess(emailProperties: EmailProperties, handler: ProcessInstanceHandler) {
+        if (emailProperties.isMarkAsRead) {
+            emailService.markMailAsRead(handler.emailSubject, handler.emailLabel, handler.receivedDate, emailProperties)
+        }
     }
 
-    private fun processScanSuccess(configurationForUser: UserConfiguration, handler: ProcessInstanceHandler) {
-        val scanProperties = configurationForUser.scanProperties
+    private fun processScanSuccess(scanProperties: ScanProperties, handler: ProcessInstanceHandler) {
         val scanMoved = scanService.moveScan(handler.fileName,
                                              handler.fileContent,
                                              scanProperties,

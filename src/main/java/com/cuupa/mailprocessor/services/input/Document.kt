c1d17fe1865b48feae23f9fc5ebf8da7d7e56b09
simon <simon.thiel2+github@gmail.com>
2020-08-01 18:13:01 +0200

    implemented equals and hashCode

--- src/main/java/com/cuupa/mailprocessor/services/input/Document.kt
+++ src/main/java/com/cuupa/mailprocessor/services/input/Document.kt
@@ -6,4 +6,29 @@ open class Document : Serializable {
     lateinit var user: String
     lateinit var filename: String
     var content: ByteArray? = null
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            null -> false
+            !is Document -> false
+            other.user == user && other.filename == filename && contentEquals(other) -> true
+            else -> false
+        }
+    }
+
+    private fun contentEquals(other: Document): Boolean {
+        return when {
+            other.content == null && content == null -> true
+            other.content != null && content == null -> false
+            other.content != null -> other.content!!.contentEquals(content!!)
+            else -> false
+        }
+    }
+
+    override fun hashCode(): Int {
+        var result = user.hashCode()
+        result = 31 * result + filename.hashCode()
+        result = 31 * result + (content?.contentHashCode() ?: 0)
+        return result
+    }
 }
--- src/main/java/com/cuupa/mailprocessor/services/input/EMail.kt
+++ src/main/java/com/cuupa/mailprocessor/services/input/EMail.kt
@@ -11,5 +11,37 @@ class EMail : Document() {
     var attachments = mutableListOf<Attachment>()
 
     val isValid
-        get() = !from.isNullOrEmpty() && receivedDate != null && (content?.isNotEmpty()?: false)
+        get() = !from.isNullOrEmpty() && receivedDate != null && (content?.isNotEmpty() ?: false)
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            null -> false
+            !is EMail -> false
+            equals(other) && super.equals(other) -> true
+            else -> false
+        }
+    }
+
+    private fun equals(
+            other: EMail) = fromEquals(other) && subjectEquals(other) && labelEquals(other) && receivedDateEquals(other) && attachmentsEquals(
+        other)
+
+    private fun attachmentsEquals(other: EMail) = attachments == other.attachments
+
+    private fun receivedDateEquals(other: EMail) = other.receivedDate == receivedDate
+
+    private fun labelEquals(other: EMail) = other.label == label
+
+    private fun subjectEquals(other: EMail) = other.subject == subject
+
+    private fun fromEquals(other: EMail) = other.from == from
+
+    override fun hashCode(): Int {
+        var result = from?.hashCode() ?: 0
+        result = 31 * result + (subject?.hashCode() ?: 0)
+        result = 31 * result + (label?.hashCode() ?: 0)
+        result = 31 * result + (receivedDate?.hashCode() ?: 0)
+        result = 31 * result + attachments.hashCode()
+        return result
+    }
 }
